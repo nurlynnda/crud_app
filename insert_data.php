2fc420c4a9ce3f5b17d42d23a8986fba194bd78f
nurlynnda <nurlynnda@graduate.utm.my>
2024-01-07 21:40:32 +0800

    Initial commit

---
+++ insert_data.php
@@ -0,0 +1,27 @@
+<?php
+include 'dbcon.php';
+if(isset($_POST['add_student'])){
+    $f_name = $_POST['f_name'];
+    $l_name = $_POST['l_name'];
+    $age = $_POST['age'];
+
+    if($f_name == "" || empty($f_name)){
+        header('location:index.php?message=You need to fill in the first name!');
+    }
+
+    else{
+
+        $query = "insert into `students` (`first_name`, `last_name`, `age`) values('$f_name', '$l_name', '$age')";
+
+        $result = mysqli_query($connection, $query);
+
+        if(!$result){
+            die("Questy Failed".mysqli_error());
+        }
+        else{
+            header('location:index.php?insert_msg=Your data has been added successfully');
+        }
+    }
+
+}
+?>
